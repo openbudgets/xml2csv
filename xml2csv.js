48b459774b4162103b73cb498992c81d465c542c
wk0206 <wk0206@gmail.com>
2016-10-05 10:23:48 +0200

    bug fix

--- xml2csv.js
+++ xml2csv.js
@@ -7,7 +7,7 @@ var requestHandlers = require("./requestHandlers");
 program
     .version('0.0.1')
     .usage('[options] <keywords>')
-    .option('-a, --add', 'should be 1,3,5,13,15,35,135')
+    .option('-a, --add', 'should be 1,3,5,7')
 	.option('-l, --log', 'output log')
     .parse(process.argv);
 
@@ -44,6 +44,7 @@ if(!program.args.length) {
 
 				//console.log("body " + body);
 				//var body = JSON.parse(body);
+				console.log("online "+ url.toString());
 				if(program.full) {
 					//console.log(body);
 				} else {
@@ -76,45 +77,59 @@ if(!program.args.length) {
 			}
 		});
 	}else{
-		//console.log("local "+ url.toString());
+		console.log("local "+ url.toString());
+
 		fs = require('fs')
-		fs.readFile(url, 'utf8', function (error,body) {
-			if (!error ) {
 
-				//console.log("body " + body);
-				//var body = JSON.parse(body);
-				if(program.full) {
-					//console.log(body);
-				} else {
-					//console.log("withLog"+withLog);
-					//1 = attribute
-					//3 = single brother
-					//5 = combine similar
-					//7 = omit same value
-					var treatXML  = requestHandlers.treatXMLFile(body,additionOption,withLog);
+		fs.stat(url.toString(),function (error){
+			if(!error){
+				console.log("file exist");
 
-					var temp = requestHandlers.outputXML(treatXML,url,additionOption,withLog);
+				fs.readFile(url, 'utf8', function (error,body) {
+					if (!error ) {
+
+						//console.log("body " + body);
+						//var body = JSON.parse(body);
+						if(program.full) {
+							//console.log(body);
+						} else {
+							//console.log("withLog"+withLog);
+							//1 = attribute
+							//3 = single brother
+							//5 = combine similar
+							//7 = omit same value
+							var treatXML  = requestHandlers.treatXMLFile(body,additionOption,withLog);
+
+							var temp = requestHandlers.outputXML(treatXML,url,additionOption,withLog);
 
 
-					//for(var i = 0; i < body.items.length; i++) {
-					//console.log(chalk.cyan.bold('Name: ' + body.items[i].name));
-					//console.log(chalk.magenta.bold('Owner: ' + body.items[i].owner.login));
-					//console.log(chalk.grey('Desc: ' + body.items[i].description + '\n'));
-					//console.log(chalk.grey('Clone url: ' + body.items[i].clone_url + '\n'));
+							//for(var i = 0; i < body.items.length; i++) {
+							//console.log(chalk.cyan.bold('Name: ' + body.items[i].name));
+							//console.log(chalk.magenta.bold('Owner: ' + body.items[i].owner.login));
+							//console.log(chalk.grey('Desc: ' + body.items[i].description + '\n'));
+							//console.log(chalk.grey('Clone url: ' + body.items[i].clone_url + '\n'));
 
-					//}
+							//}
 
-					process.exit(0);
-					return temp;
-				}
-				process.exit(0);
-			} else if (error) {
-				//console.log(chalk.red('Error: ' + error));
-				console.log('Error: ');
+							process.exit(0);
+							return temp;
+						}
+						process.exit(0);
+					} else if (error) {
+						//console.log(chalk.red('Error: ' + error));
+						console.log('Error: '+error.toString());
+						process.exit(1);
+					}
+					//console.log(data);
+				});
+
+			}else {
+				console.log("file do not exist");
 				process.exit(1);
 			}
-			//console.log(data);
-		});
+		})
+
+
 	}
 
     //console.log('Keywords: ' + program.args);
